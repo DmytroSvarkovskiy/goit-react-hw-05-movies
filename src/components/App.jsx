c98f74eb4246b62498c5f7e55551a3f43cbab760
Дмитро Сварковський <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2022-12-18 17:28:37 +0200

    -

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,8 @@
+import { GlobalStyle } from './GlobalStyled';
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    ></div>
+    <div>
+      <GlobalStyle />
+    </div>
   );
 };
